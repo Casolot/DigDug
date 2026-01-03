499d5bcd85c7c4c55680ba93ff629fc591df60bc
Casolot <gene.trousselot@gmail.com>
2026-01-03 15:53:53 +0900

    fix: ダメージポップアップの表示順を調整

--- src/App.tsx
+++ src/App.tsx
@@ -266,9 +266,9 @@ export default function App() {
                 className={`damagePopup damagePopup--${p.kind} damagePopup--${p.tone}`}
                 style={{ left: p.x, top: p.y, "--dmg-scale": p.scale } as React.CSSProperties}
               >
-                {p.label && <span className="damagePopup__label">{p.label}</span>}
                 <span className="damagePopup__num">{p.dmg}</span>
                 <span className="damagePopup__suffix">ダメージ！</span>
+                {p.label && <span className="damagePopup__label">{p.label}</span>}
               </div>
             ))}
           </section>
